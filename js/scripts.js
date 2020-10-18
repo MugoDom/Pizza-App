0fa92e1ea51a57c6af0472662ce9bc76b564d384
Dominic Mugo <njuemugodominic@gmail.com>
2020-10-18 12:41:41 +0300

    create a switch function

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,235 @@ $("#checkout").click(function(){
     let crust = $("#crust option:selected").val();
     let numberOrder = $("number option:selected").val();
 
-    console.log(topping);
+
+    let order = (size, topping, crust, numberOrder, total) => {
+        return {size, topping, crust, numberOrder, total};
+    };
+
+
+    //checking the prices of customer selection
+    let price, totalPrice;
+    switch(topping){
+        case topping = "pepperoni":
+            switch(size){
+                case size = "small":
+                    price = 500;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "medium":
+                    price = 750;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "large":
+                    price = 1000;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+
+            }
+            break;
+        case topping = "mushroom":
+            switch (swize){
+                case size = "small":
+                    price = 500;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "medium":
+                    price = 750;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "large":
+                    price = 1000;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+            }
+            break;
+        case topping = "bacon":
+            switch (size) {
+                case size = "small":
+                    price = 500;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "medium":
+                    price = 750;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "large":
+                    price = 1000;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+            }
+            break;
+
+        case topping = "greenpeppers":
+            switch (size){
+                case size = "small":
+                    price = 500;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "medium":
+                    price = 750;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "large":
+                    price = 1000;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+            }
+            break;
+
+        case topping = "cheese":
+            switch (size){
+                case size = "small":
+                    price = 500;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "medium":
+                    price = 750;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+                case size = "large":
+                    price = 1000;
+                    if (crust === "chicago"){
+                        totalPrice = (price * number) + 200;
+                    } else if (crust === "sicilian"){
+                        totalPrice = (price * number) + 250;
+                    } else if (crust === "stuffed"){
+                        totalPrice = (price * number) + 150;
+                    } else {
+                        totalPrice = (price * number) + 100;
+                    }
+                    break;
+
+            }
+    }
+
+    let newOrder = order(size, topping, crust, number, totalPrice);
+    console.log(newOrder);
 
 });
